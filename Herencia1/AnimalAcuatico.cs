f2e4233a3a6bc90a5c68d0b3866cc96383a6b257
Alumno Ulsa <alumno@ulsa.ulsa-noroeste.edu.mx>
2018-11-15 08:23:02 -0700

    Agregar archivos de proyecto.

---
+++ Herencia1/AnimalAcuatico.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia1
+{
+    public class AnimalAcuatico : Animal1
+    {
+        public void Nadar()
+        {
+            Console.WriteLine("Hola soy un " + Nombre + " y estoy nadando");
+        }
+    }
+}
